16f0e55fb8ab45f104d51edc2c1e2cdd78d7c2f5
Pavel Chernyshev <chernyshev@kolesa.kz>
2022-10-04 15:18:26 +0600

    Обновляю статью и добавляю роутер

--- public/index.php
+++ public/index.php
@@ -2,8 +2,24 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Application;
 
 $app = new Application(dirname(__DIR__));
+
+$app->router->get('/', function () {
+    return 'Hello Kolesa Backend Upgrade!';
+});
+
+$app->router->get('/hello', function (Request $request) {
+    $name = $request->input('name', 'guest');
+
+    return 'Hello ' . $name;
+});
+
+$app->router->get('/server', function () {
+    // Пример протечки абстракции - прямое обращение к глобальной переменной
+    return response()->json($_SERVER);
+});
 
 $app->run();
